df361240379ed3e8f270b6cb11314f4406467ca0
syafiqntu <42940943+syafiqntu@users.noreply.github.com>
2018-10-17 02:18:44 +0800

    Revert "first website"
    This reverts commit 85666e7acfa39384c60fc8e8f05b26919389ebb5.

--- mysite/personal/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from . import views #new
-
-
-urlpatterns = [
-    path('', views.index, name='index' ),
-    path('contact/', views.contact, name='contact'),
-]
